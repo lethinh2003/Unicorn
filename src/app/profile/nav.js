0c62af06ecc6c5273249d08673ef67f473a57332
ZzinzZ <nhata338@gmail.com>
2023-10-07 18:13:50 +0700

    Profile and address page final commit

--- src/app/profile/nav.js
+++ src/app/profile/nav.js
@@ -13,30 +13,35 @@ import { useState, useEffect } from "react";
 import { usePathname } from "next/navigation";
 import Infomation from "./infomation";
 
-const ToggleButtons = [
+const IconArray = [
   {
-    path: "/profile",
     label: "Tài khoản của tôi",
+    path: "/profile",
+    icon: <AccountCircleOutlinedIcon />,
     value: "user-infomation",
   },
   {
-    path: "/profile/address",
     label: "Địa chỉ",
+    path: "/profile/address",
+    icon: <PersonPinCircleOutlinedIcon />,
     value: "user-location",
   },
   {
-    path: "/profile/tickets",
     label: "Mã giảm giá",
+    path: "/profile/tickets",
+    icon: <ConfirmationNumberOutlinedIcon />,
     value: "user-ticket",
   },
   {
-    path: "/profile/history",
     label: "Lịch sử đơn hàng",
+    path: "/profile/history",
+    icon: <HistoryOutlinedIcon />,
     value: "user-history",
   },
   {
-    path: "/profile/notifications",
     label: "Thông báo",
+    path: "/profile/notifications",
+    icon: <NotificationsActiveOutlinedIcon />,
     value: "user-notification",
   },
 ];
@@ -46,65 +51,53 @@ export default function ProfileNav() {
   const pathName = usePathname();
 
   return (
-    <>
-      <div className="container">
-        <div className="user-nav-container">
-          <div className="user-nav">
-            <div className="user-avt-logout">
-              <Image
-                src="/use-logo.png"
-                alt="me"
-                width="40"
-                height="40"
-                className="user-avt"
-              ></Image>
-              <span className="user-name">Na</span>
-              <Button className="logout-button">Đăng xuất</Button>
-            </div>
-            <div className="nav-infomation">
-              <ToggleButtonGroup
-                orientation="vertical"
-                value={view}
-                exclusive
-                sx={{ width: "100%" }}
-              >
-                {ToggleButtons.map((toggle, index) => (
-                  <Link href={toggle.path} key={index}>
-                    <ToggleButton
-                      value={toggle.value}
-                      aria-label="list"
-                      sx={{
-                        backgroundColor:
-                          pathName === toggle.path
-                            ? "#C0DED7 !important"
-                            : "transparent",
-                        borderLeft: "none",
-                        borderRight: "none",
-                        width: "100%",
-                      }}
-                    >
-                      <div className="nav-toggle-content">
-                        {toggle.path === "/profile" ? (
-                          <AccountCircleOutlinedIcon />
-                        ) : toggle.path === "/profile/address" ? (
-                          <PersonPinCircleOutlinedIcon />
-                        ) : toggle.path === "/profile/tickets" ? (
-                          <ConfirmationNumberOutlinedIcon />
-                        ) : toggle.path === "/profile/history" ? (
-                          <HistoryOutlinedIcon />
-                        ) : toggle.path === "/profile/notifications" ? (
-                          <NotificationsActiveOutlinedIcon />
-                        ) : null}
-                        <p className="toggle-button-text">{toggle.label}</p>
-                      </div>
-                    </ToggleButton>
-                  </Link>
-                ))}
-              </ToggleButtonGroup>
-            </div>
+    <div className="container">
+      <div className="user-nav-container">
+        <div className="user-nav">
+          <div className="user-avt-logout">
+            <Image
+              src="/use-logo.png"
+              alt="me"
+              width="40"
+              height="40"
+              className="user-avt"
+            ></Image>
+            <span className="user-name">Na</span>
+            <Button className="logout-button">Đăng xuất</Button>
+          </div>
+          <div className="nav-infomation">
+            <ToggleButtonGroup
+              orientation="vertical"
+              value={view}
+              exclusive
+              sx={{ width: "100%" }}
+            >
+              {IconArray.map((toggle, index) => (
+                <Link href={toggle.path} key={index}>
+                  <ToggleButton
+                    value={toggle.value}
+                    aria-label="list"
+                    sx={{
+                      backgroundColor:
+                        pathName === toggle.path
+                          ? "#C0DED7 !important"
+                          : "transparent",
+                      borderLeft: "none",
+                      borderRight: "none",
+                      width: "100%",
+                    }}
+                  >
+                    <div className="nav-toggle-content">
+                      {toggle.icon}
+                      <p className="toggle-button-text">{toggle.label}</p>
+                    </div>
+                  </ToggleButton>
+                </Link>
+              ))}
+            </ToggleButtonGroup>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
