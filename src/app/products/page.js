b4716ac408e5d3bea939712a5a650161a4e42635
lethinh2003 <muradvn2003@gmail.com>
2023-10-23 14:59:09 +0700

    complete get all products with responsive, skeleton loading, pre fetch filter on server

--- src/app/products/page.js
+++ src/app/products/page.js
@@ -1,111 +1,42 @@
-"use client";
-import { LoadingContent } from "@/components/generals/LoadingBox";
-import { AllProductItem } from "@/components/product/ProductItem";
-import {
-  Box,
-  Breadcrumbs,
-  Button,
-  Grid,
-  Stack,
-  Typography,
-} from "@mui/material";
+import { Box, Breadcrumbs, Typography } from "@mui/material";
 import axios from "axios";
-import _ from "lodash";
 import Image from "next/image";
 import Link from "next/link";
-import { useSearchParams } from "next/navigation";
-import { useEffect, useState } from "react";
-import { useInfiniteQuery } from "react-query";
-import Filter from "./filter";
-const ITEMS_OF_PAGE = 50;
-export default function Products() {
-  const searchParams = useSearchParams();
-  const searchColor = searchParams.get("color");
-  const searchSize = searchParams.get("size");
-  const searchCategory = searchParams.get("category");
-  const searchGender = searchParams.get("gender");
-  const [itemsPerPage, setItemsPerPage] = useState(ITEMS_OF_PAGE);
+import Filter from "./Filter";
+import Products from "./Products";
 
-  const [filterValue, setFilterValue] = useState({
-    category: "all",
-    color: "all",
-    size: "all",
-    gender: "unisex",
-  });
-
-  const getListProducts = async (pageParam) => {
-    const results = await axios.get(
-      `${process.env.NEXT_PUBLIC_ENDPOINT_SERVER}/api/v1/products?category=${filterValue.category}&gender=${filterValue.gender}&size=${filterValue.size}&color=${filterValue.color}&page=${pageParam}&itemsOfPage=${itemsPerPage}`
+const getDataFilter = async ({ gender }) => {
+  try {
+    const getCategories = axios.get(
+      `${process.env.NEXT_PUBLIC_ENDPOINT_SERVER}/api/v1/product-categories?gender=${gender}`
     );
-    return results.data;
-  };
+    const getColors = axios.get(
+      `${process.env.NEXT_PUBLIC_ENDPOINT_SERVER}/api/v1/product-colors`
+    );
+    const getSizes = axios.get(
+      `${process.env.NEXT_PUBLIC_ENDPOINT_SERVER}/api/v1/product-sizes`
+    );
+    const [categoriesData, colorsData, sizesData] = await Promise.all([
+      getCategories,
+      getColors,
+      getSizes,
+    ]);
 
-  const getListQuery = useInfiniteQuery(
-    ["get-list-product", filterValue],
-    ({ pageParam = 1 }) => getListProducts(pageParam),
-    {
-      getNextPageParam: (_lastPage, pages) => {
-        if (pages[pages.length - 1].metadata.results === itemsPerPage) {
-          return pages.length + 1;
-        }
-        return undefined;
-      },
-    }
-  );
-  const {
-    data: dataProducts,
-    isLoading: isLoadingQuery,
-    isFetching,
-    isError: isErrorQuery,
-    error,
-    hasNextPage,
-    isFetchingNextPage,
-    fetchNextPage,
-  } = getListQuery;
-  useEffect(() => {
-    if (isErrorQuery) {
-      throw error;
-    }
-  }, [isErrorQuery]);
-
-  useEffect(() => {
-    setFilterValue((value) => ({
-      ...value,
-      color: !searchColor ? "all" : searchColor,
-      size: !searchSize ? "all" : searchSize,
-      category: !searchCategory ? "all" : searchCategory,
-      gender: !searchGender ? "unisex" : searchGender,
-    }));
-  }, [searchColor, searchSize, searchCategory, searchGender]);
-
-  // handle merge child product to parent product
-  const products = dataProducts?.pages.flatMap((page) => page.data) || [];
-  const finalProducts = [];
-  const objMapProducts = _.keyBy(products, "_id");
-  for (const product of products) {
-    if (filterValue.color !== "all") {
-      finalProducts.push(product);
-    } else {
-      if (!product.parent_product_id) {
-        finalProducts.push(product);
-      } else {
-        // find Parent Product
-        const parentProduct = objMapProducts[product.parent_product_id];
-        if (parentProduct) {
-          if (!parentProduct.child_products) {
-            parentProduct.child_products = [];
-          } else {
-            parentProduct.child_products.push(product);
-          }
-          parentProduct.child_products = _.uniqBy(
-            parentProduct.child_products,
-            "_id"
-          );
-        }
-      }
-    }
+    return [
+      categoriesData.data.data,
+      colorsData.data.data,
+      sizesData.data.data,
+    ];
+  } catch (err) {
+    throw err;
   }
+};
 
+export default async function ProductsPage({ searchParams }) {
+  const { gender, category, color, size } = searchParams;
+  const filterData = await getDataFilter({
+    gender,
+  });
   return (
     <div className="product-container">
       <div className="product-header">
@@ -115,22 +46,14 @@ export default function Products() {
               Trang chủ
             </Link>
             <Typography color="text.primary">
-              {searchGender === "men"
-                ? "Nam"
-                : searchGender === "women"
-                ? "Nữ"
-                : ""}{" "}
+              {gender === "men" ? "Nam" : gender === "women" ? "Nữ" : ""}{" "}
             </Typography>
           </Breadcrumbs>
         </div>
         <div className="product-page-title">
           <h1 className="product-page-title-text">
             Thời trang{" "}
-            {searchGender === "men"
-              ? "Nam"
-              : searchGender === "women"
-              ? "Nữ"
-              : ""}{" "}
+            {gender === "men" ? "Nam" : gender === "women" ? "Nữ" : ""}{" "}
           </h1>
         </div>
       </div>
@@ -143,52 +66,25 @@ export default function Products() {
           className="header-image"
         />
       </div>
-      <Stack direction="row" className="product-page-body">
-        <div className="product-filter">
-          <Filter></Filter>
-        </div>
-        <div className="product-item-container">
-          <div className="products-page-header">
-            <span className="products-page-header-title">
-              Danh sách sản phẩm
-            </span>
-          </div>
-          <Grid container className="products-item-wrap">
-            {isLoadingQuery && !isFetchingNextPage && (
-              <Box
-                sx={{
-                  paddingTop: "5rem",
-                  width: "100%",
-                }}
-              >
-                <LoadingContent />
-              </Box>
-            )}
+      <Box
+        sx={{
+          gap: "2rem",
+          display: "flex",
+          flexDirection: { xs: "column", md: "row" },
+        }}
+      >
+        <Box
+          className="product-filter"
+          sx={{
+            position: "sticky",
+            left: 0,
+          }}
+        >
+          <Filter filterData={filterData} />
+        </Box>
 
-            {finalProducts.map((product) => (
-              <AllProductItem key={product._id} product={product} />
-            ))}
-          </Grid>
-
-          {/* <div className="viewmore">
-            <Button className="viewmore-button">Xem thêm</Button>
-          </div> */}
-
-          {isFetchingNextPage && <LoadingContent />}
-          {hasNextPage && !isFetchingNextPage && (
-            <Box
-              sx={{
-                paddingTop: "10px",
-                textAlign: "center",
-              }}
-            >
-              <Button variant="contained" onClick={() => fetchNextPage()}>
-                Tải thêm
-              </Button>
-            </Box>
-          )}
-        </div>
-      </Stack>
+        <Products />
+      </Box>
     </div>
   );
 }
--- src/components/product/ProductItem.js
+++ src/components/product/ProductItem.js
@@ -1,11 +1,19 @@
 import { ConvertMoney } from "@/utils/convertMoney";
 import { Favorite, FavoriteBorder } from "@mui/icons-material";
-import { Box, Checkbox, Stack, Typography } from "@mui/material";
+import { Box, Checkbox, Skeleton, Stack, Typography } from "@mui/material";
 import Image from "next/image";
+import Link from "next/link";
+import { useState } from "react";
 export const AllProductItem = ({ product }) => {
+  const [mainImage, setMainImage] = useState(product.product_images[0]);
   return (
     <>
-      <Stack className="products-item">
+      <Stack
+        className="products-item"
+        sx={{
+          cursor: "default",
+        }}
+      >
         <Checkbox
           icon={<FavoriteBorder />}
           checkedIcon={<Favorite sx={{ color: "red" }} />}
@@ -16,8 +24,8 @@ export const AllProductItem = ({ product }) => {
           }}
         />
         <Image
-          src={product.product_images[0]}
-          alt=""
+          src={mainImage}
+          alt={product.product_name}
           width={500}
           height={500}
           className="product-item-image"
@@ -26,42 +34,121 @@ export const AllProductItem = ({ product }) => {
           className="product-details"
           style={{
             gap: "1rem",
+            height: "20rem",
           }}
         >
-          <span className="product-item-name">{product.product_name}</span>
+          <Link href={`/products/${product._id}`}>
+            <span
+              title={product.product_name}
+              className="product-item-name cut-text"
+            >
+              {product.product_name}
+            </span>
+          </Link>
+
           <span className="product-item-price">
             {<ConvertMoney money={product.product_original_price} />}đ
           </span>
-          <div className="home-product__colors" style={{ display: "flex" }}>
+          <Box
+            className="home-product__colors"
+            sx={{
+              display: "flex",
+              flexWrap: "wrap",
+            }}
+          >
             <div
+              onClick={() => setMainImage(product.product_images[0])}
               style={{
+                cursor: "pointer",
                 border: "1px solid",
                 width: "2rem",
                 height: "2rem",
                 marginRight: "1rem",
+                marginTop: "1rem",
                 backgroundColor: product?.product_color.product_color_code,
               }}
             ></div>
             {product?.child_products?.map((childProduct) => {
               return (
                 <div
+                  key={childProduct._id}
+                  onClick={() => setMainImage(childProduct.product_images[0])}
                   style={{
+                    cursor: "pointer",
                     border: "1px solid",
                     width: "2rem",
                     height: "2rem",
                     marginRight: "1rem",
+                    marginTop: "1rem",
                     backgroundColor:
                       childProduct?.product_color.product_color_code,
                   }}
                 ></div>
               );
             })}
-          </div>
+          </Box>
         </div>
       </Stack>
     </>
   );
 };
+export const SkeletonAllProductItem = ({}) => {
+  return (
+    <>
+      <Stack
+        className="products-item"
+        sx={{
+          cursor: "default",
+
+          maxWidth: "30rem",
+          width: "100%",
+        }}
+      >
+        <Skeleton
+          variant="rectangular"
+          height={250}
+          sx={{
+            width: "100%",
+          }}
+          className="product-item-image"
+        />
+
+        <div
+          className="product-details"
+          style={{
+            gap: "1rem",
+            height: "20rem",
+          }}
+        >
+          <Skeleton
+            variant="text"
+            sx={{ fontSize: "1rem", height: "7rem" }}
+            className="product-item-name cut-text"
+          />
+          <Skeleton
+            variant="text"
+            sx={{ fontSize: "1rem", width: "7rem", height: "3rem" }}
+            className="product-item-price"
+          />
+
+          <Box
+            className="home-product__colors"
+            sx={{
+              display: "flex",
+              flexWrap: "wrap",
+              gap: "1rem",
+            }}
+          >
+            {Array.from({ length: 5 }).map((_item, i) => (
+              <Skeleton key={i} variant="rectangular" width={20} height={20} />
+            ))}
+          </Box>
+        </div>
+      </Stack>
+    </>
+  );
+};
+
 export const ProductItem = ({ product }) => {
   return (
     <>
@@ -99,19 +186,24 @@ export const ProductItem = ({ product }) => {
             }}
           />
 
-          <div className="home-product__colors" style={{ display: "flex" }}>
+          <div
+            className="home-product__colors"
+            style={{ display: "flex", flexWrap: "wrap" }}
+          >
             <div
               style={{
                 border: "1px solid",
                 width: "2rem",
                 height: "2rem",
                 marginRight: "1rem",
+                marginTop: "1rem",
                 backgroundColor: product?.product_color.product_color_code,
               }}
             ></div>
             {product?.child_products?.map((childProduct) => {
               return (
                 <div
+                  key={childProduct._id}
                   style={{
                     border: "1px solid",
                     width: "2rem",
