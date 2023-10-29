48ce45df7b1c08790e2c45fee5e2d2f988f4fc2f
ZzinzZ <nhata338@gmail.com>
2023-10-29 15:14:37 +0700

    fixed add user page final

--- src/app/(admin)/admin/users/add/page.js
+++ src/app/(admin)/admin/users/add/page.js
@@ -10,6 +10,7 @@ import { Stack, Input, Button } from '@mui/material';
 import * as React from 'react';
 import MenuItem from '@mui/material/MenuItem';
 import Select from '@mui/material/Select';
+import HeaderTitle from '../../adminComponent/headerTitle';
 
 const ADD_USERS = [
   {
@@ -26,8 +27,8 @@ const ADD_USERS = [
 
 export default function AddUsers() {
   const [addusers, setAddUsers] = useState(ADD_USERS);
-  const [gender, setGender] = React.useState('');
-  const [vaitro, setVaiTro] = React.useState('');
+  const [gender, setGender] = useState('');
+  const [role, setRole] = useState('');
 
 
 
@@ -48,181 +49,192 @@ export default function AddUsers() {
 
 
   return (
-
-    <div className='admin-user-add'>
-      <div className='admin-users-add-basicInformation'>
-        {addusers.map((addusers, index) => (
-          <Stack
-            key={index}
-            className='input-col'
-            sx={{
-              textAlign: "start",
-              fontSize: "1.5rem",
-              gap: "1.5rem",
-              width:'100%',
-            }}
-          >
-              <div className='admin-users-add-title'>{addusers.title}</div>
-              <Box
+    <>
+      <HeaderTitle></HeaderTitle>
+      <div className="admin-user-add">
+        <div className="admin-user-add-form">
+          <div className="admin-users-add-basicInformation">
+            {addusers.map((addusers, index) => (
+              <Stack
+                key={index}
+                className="input-col"
                 sx={{
-                  
-                  width: '100%',
+                  textAlign: "start",
+                  fontSize: "1.5rem",
+                  gap: "1.5rem",
+                  width: "100%",
                 }}
               >
-                <TextField fullWidth id="fullWidth" sx={{background:'#EEE'}} />
+                <div className="admin-users-add-title">{addusers.title}</div>
+                <Box
+                  sx={{
+                    width: "100%",
+                  }}
+                >
+                  <TextField fullWidth id="fullWidth" />
+                </Box>
+              </Stack>
+            ))}
+          </div>
+
+          <div className="admin-users-add-other">
+            <Stack
+              direction="row"
+              display="flex"
+              gap="10.1rem"
+              marginBottom="2rem"
+            >
+              <Stack
+                className="input-col"
+                sx={{
+                  textAlign: "start",
+                  fontSize: "1.5rem",
+                  gap: "1.5rem",
+                  width: "100%",
+                }}
+              >
+                <div className="admin-users-add-title">Mật khẩu</div>
+                <Box
+                  sx={{
+                    width: "100%",
+                  }}
+                >
+                  <TextField fullWidth id="fullWidth" />
+                </Box>
+              </Stack>
+
+              <Stack
+                className="input-col"
+                sx={{
+                  textAlign: "start",
+                  fontSize: "1.5rem",
+                  gap: "1.5rem",
+                  width: "100%",
+                }}
+              >
+                <div className="admin-users-add-title">Trạng thái</div>
+                <Box
+                  sx={{
+                    width: "100%",
+                  }}
+                >
+                  <TextField fullWidth id="fullWidth" />
+                </Box>
+              </Stack>
+            </Stack>
+
+            <Stack
+              direction="row"
+              display="flex"
+              gap="10.1rem"
+              marginBottom="2rem"
+            >
+              <Stack
+                className="input-col"
+                sx={{
+                  textAlign: "start",
+                  fontSize: "1.5rem",
+                  gap: "1.5rem",
+                  marginBottom: "2rem",
+                  width: "100%",
+                }}
+              >
+                <div className="admin-users-add-title">Giới tính</div>
+                <Box>
+                  <FormControl fullWidth>
+                    <Select
+                      labelId="demo-simple-select-label"
+                      id="demo-simple-select"
+                      value={gender}
+                      onChange={handleChange}
+                    >
+                      <MenuItem value={"nam"}>Nam</MenuItem>
+                      <MenuItem value={"nữ"}>Nữ</MenuItem>
+                      <MenuItem value={"khác"}>Khác</MenuItem>
+                    </Select>
+                  </FormControl>
+                </Box>
+              </Stack>
+
+              <Stack
+                className="input-col"
+                sx={{
+                  textAlign: "start",
+                  fontSize: "1.5rem",
+                  gap: "1.5rem",
+                  width: "100%",
+                }}
+              >
+                <div className="admin-users-add-title">Vai trò</div>
+                <Box sx={{ minWidth: "40rem" }}>
+                  <FormControl fullWidth>
+                    <Select
+                      labelId="demo-simple-select-label"
+                      id="demo-simple-select"
+                      value={role}
+                      onChange={handleChange}
+                    ></Select>
+                  </FormControl>
+                </Box>
+              </Stack>
+            </Stack>
+
+            <Stack
+              className="input-col"
+              sx={{
+                textAlign: "start",
+                fontSize: "1.5rem",
+                gap: "1.5rem",
+                width: "100%",
+              }}
+            >
+              <Box
+                component="form"
+                sx={{
+                  "& .MuiTextField-root": { marginLeft: 0, width: "100%" },
+                  width: "100%",
+                  gap: "5rem",
+                  display: "flex",
+                  flexdirection: "row",
+                }}
+                noValidate
+                autoComplete="off"
+              >
+                <TextField
+                  id="outlined-basic"
+                  label="Ngày"
+                  variant="outlined"
+                />
+                <TextField id="filled-basic" label="Tháng" variant="outlined" />
+                <TextField id="filled-basic" label="Năm" variant="outlined" />
               </Box>
-          </Stack>
-        ))}
-      </div>
+            </Stack>
 
-      <div className='admin-users-add-other'>
-        <Stack direction="row" display="flex" gap="10.1rem" marginBottom="2rem">
-        <Stack
-          className='input-col'
-          sx={{
-            textAlign: "start",
-            fontSize: "1.5rem",
-            gap: "1.5rem",
-            width:'100%',
-          }}
-        >
-          <div className='admin-users-add-title'>Mật khẩu</div>
-          <Box
-            sx={{
-              width: '100%',
-              background:'#EEE',
-            }}
-          >
-            <TextField fullWidth id="fullWidth" />
-          </Box>
-
-        </Stack>
-
-        <Stack
-          className='input-col'
-          sx={{
-            textAlign: "start",
-            fontSize: "1.5rem",
-            gap: "1.5rem",
-            width:'100%',
-          }}
-        >
-              <div className='admin-users-add-title'>Trạng thái</div>
-            <Box
-            sx={{
-              width:'100%',
-              background:'#EEE',
-            }}
-          >
-            <TextField fullWidth id="fullWidth" />
-          </Box>
-        </Stack>
-        </Stack>
-
-        <Stack direction="row" display="flex" gap="10.1rem" marginBottom="2rem">
-        <Stack
-          className='input-col'
-          sx={{
-            textAlign: "start",
-            fontSize: "1.5rem",
-            gap: "1.5rem",
-            marginBottom:"2rem",
-            width:'100%',
-          }}
-        >
-          <div className='admin-users-add-title'>Giới tính</div>
-          <Box>
-            <FormControl fullWidth>
-              <Select
-                labelId="demo-simple-select-label"
-                id="demo-simple-select"
-                value={gender}
-                onChange={handleChange}
-                sx={{background:'#EEE'}}
+            <Stack
+              spacing="4.9rem"
+              direction="row"
+              marginTop="7rem"
+              justifyContent="flex-end"
+            >
+              <Button
+                variant="outlined"
+                sx={{
+                  border: "1px solid #ACA3A3",
+                  background: " #FFF",
+                  color: "#000",
+                  fontsize: "2rem",
+                  fontstyle: "normal",
+                  fontweight: "600",
+                  lineheight: "normal",
+                }}
               >
-                <MenuItem value={'nam'}>Nam</MenuItem>
-                <MenuItem value={'nữ'}>Nữ</MenuItem>
-                <MenuItem value={'khác'}>Khác</MenuItem>
-              </Select>
-            </FormControl>
-          </Box>
-        </Stack>
-
-        <Stack
-          className='input-col'
-          sx={{
-            textAlign: "start",
-            fontSize: "1.5rem",
-            gap: "1.5rem",
-            width:'100%',
-
-          }}
-        >
-          <div className='admin-users-add-title'>Vai trò</div>
-          <Box sx={{ minWidth: '40rem' }}>
-            <FormControl fullWidth>
-              <Select
-                labelId="demo-simple-select-label"
-                id="demo-simple-select"
-                value={vaitro}
-                onChange={handleChange}
-                sx={{background:'#EEE'}}
-              >
-                
-              </Select>
-            </FormControl>
-          </Box>
-        </Stack>
-        </Stack>
-
-        <Stack
-          className='input-col'
-          sx={{
-            textAlign: "start",
-            fontSize: "1.5rem",
-            gap: "1.5rem",
-            width:'100%',
-          }}
-        >
-          <Box
-            component="form"
-            sx={{
-              '& .MuiTextField-root': { marginLeft:0, width: '100%' },
-              width:'100%',
-              gap:'5rem',
-              display: 'flex',
-              flexdirection: 'row',
-            }}
-            noValidate
-            autoComplete="off"
-            
-
-          >
-            <TextField id="outlined-basic" label="Ngày" variant="outlined" sx={{background:'#EEE'}} />
-            <TextField id="filled-basic" label="Tháng" variant="outlined" sx={{background:'#EEE'}} />
-            <TextField id="filled-basic" label="Năm" variant="outlined" sx={{background:'#EEE'}} />
-          </Box>
-        </Stack>
-
-        <Stack spacing='4.9rem' direction="row" marginTop='7rem' justifyContent='flex-end'>
-          <Button variant="outlined"
-            sx={{
-              border: '1px solid #ACA3A3',
-              background:' #FFF',
-              color: '#000',
-              fontsize: '2rem',
-              fontstyle: 'normal',
-              fontweight: '600',
-              lineheight: 'normal',
-            }}    
-          >
-            Hủy
-          </Button>
-          <Button variant="contained">
-            Xác nhận
-          </Button>
-        </Stack>
+                Hủy
+              </Button>
+              <Button variant="contained">Xác nhận</Button>
+            </Stack>
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
