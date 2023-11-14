6fc58f97c386d70a9a48fd18f9f5c042f32e503d
ZzinzZ <nhata338@gmail.com>
2023-11-15 00:05:02 +0700

    fix UI recommend products and viewed product -first commit

--- src/components/product/detail/ListSuggesting.js
+++ src/components/product/detail/ListSuggesting.js
@@ -3,10 +3,20 @@ import {
   ProductItem,
   ProductItemLoading,
 } from "@/components/product/ProductItem";
-import { Box, Typography } from "@mui/material";
+import { Box, Typography, Button } from "@mui/material";
 import axios from "axios";
 import { useEffect } from "react";
 import { useQuery } from "react-query";
+import NavigateBeforeIcon from "@mui/icons-material/NavigateBefore";
+import NavigateNextIcon from "@mui/icons-material/NavigateNext";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/autoplay";
+import "swiper/css/effect-coverflow";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+import { EffectCoverflow, Navigation, Pagination } from "swiper/modules";
 const PRODUCTS = [];
 const PAGE = 1;
 const ITEMS_OF_PAGE = 5;
@@ -56,32 +66,63 @@ export default function ListSuggesting({ productId }) {
         </Box>
         <Box
           sx={{
+            position: "relative",
             display: "flex",
             gap: "1rem",
-            overflowY: "auto",
+            overflowX: "hidden",
           }}
         >
           {isLoading && (
             <>
-              {Array.from({ length: 5 }).map((_item, i) => (
+              {Array.from({ length: 3 }).map((_item, i) => (
                 <ProductItemLoading
                   sx={{
-                    minWidth: "25rem",
+                    width: "20rem",
+                    minWidth: "20rem",
                   }}
                   key={i}
                 />
               ))}
             </>
           )}
-          {data?.map((item) => (
-            <ProductItem
-              key={item._id}
-              product={item}
-              sx={{
-                minWidth: "25rem",
-              }}
-            />
-          ))}
+          <Swiper
+            grabCursor={true}
+            slidesPerView={3}
+            coverflowEffect={{
+              rotate: 0,
+              stretch: 0,
+              depth: 0,
+              modifier: 0.5,
+            }}
+            navigation={{
+              nextEl: ".recommend-button-next",
+              prevEl: ".recommend-button-prev",
+              clickable: true,
+            }}
+            pagination={{ el: "swiper-pagination", clickable: true }}
+            modules={[EffectCoverflow, Pagination, Navigation]}
+          >
+            {data?.map((item) => (
+              <SwiperSlide key={item._id}>
+                <ProductItem
+                  product={item}
+                  sx={{
+                    width: "20rem",
+                    height: "auto",
+                    minWidth: "20rem",
+                  }}
+                />
+              </SwiperSlide>
+            ))}
+          </Swiper>
+          <div className="recommend-button-controler absolute top-1/2 z-10 flex justify-between">
+            <Button className="recommend-button-prev">
+              {!isLoading ? <NavigateBeforeIcon className="" sx={{ fontSize: 50 }} /> : null}
+            </Button>
+            <Button className="recommend-button-next">
+              {!isLoading ? <NavigateNextIcon className="" sx={{ fontSize: 50 }} /> : null}
+            </Button>
+          </div>
         </Box>
       </Box>
     </>
--- src/components/product/detail/ListViewed.js
+++ src/components/product/detail/ListViewed.js
@@ -1,9 +1,19 @@
 "use client";
 import { ProductItem } from "@/components/product/ProductItem";
 import { TYPE_PRODUCT_ITEM_DISPLAY } from "@/configs/config.products";
-import { Box, Typography } from "@mui/material";
+import { Box, Typography,Button } from "@mui/material";
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
+import NavigateBeforeIcon from "@mui/icons-material/NavigateBefore";
+import NavigateNextIcon from "@mui/icons-material/NavigateNext";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/autoplay";
+import "swiper/css/effect-coverflow";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+import { EffectCoverflow, Navigation, Pagination } from "swiper/modules";
 const LIMIT_ITEMS = 10;
 export default function ListViewed() {
   const dataListViewed = useSelector((state) => state.viewedProducts);
@@ -34,19 +44,51 @@ export default function ListViewed() {
           style={{
             display: "flex",
             gap: "1rem",
-            overflowY: "auto",
+            overflowX: "hidden",
+            overflowY: "hidden",
+            position: "relative",
           }}
         >
-          {data.map((item, i) => (
-            <ProductItem
-              key={item._id}
-              product={{ ...item, child_products: item.relation_products }}
-              sx={{
-                minWidth: "25rem",
-              }}
-              type={TYPE_PRODUCT_ITEM_DISPLAY.VIEWED}
-            />
-          ))}
+          <Swiper
+            grabCursor={true}
+            slidesPerView={4}
+            coverflowEffect={{
+              rotate: 0,
+              stretch: 0,
+              depth: 0,
+              modifier: 0.5,
+            }}
+            navigation={{
+              nextEl: ".viewed-button-next",
+              prevEl: ".viewed-button-prev",
+              clickable: true,
+            }}
+            pagination={{ el: "swiper-pagination", clickable: true }}
+            modules={[EffectCoverflow, Pagination, Navigation]}
+            className="relative"
+          >
+            {data.map((item, i) => (
+              <SwiperSlide key={item._id}>
+                <ProductItem
+                  product={{ ...item, child_products: item.relation_products }}
+                  sx={{
+                    minWidth: "25rem",
+                  }}
+                  type={TYPE_PRODUCT_ITEM_DISPLAY.VIEWED}
+                />
+              </SwiperSlide>
+            ))}
+          </Swiper>
+          
+            <div className="viewed-button-controler absolute top-1/2 z-10 flex justify-between">
+              <Button className="viewed-button-prev">
+                <NavigateBeforeIcon className="" sx={{ fontSize: 50 }} />
+              </Button>
+              <Button className="viewed-button-next">
+                <NavigateNextIcon className="" sx={{ fontSize: 50 }} />
+              </Button>
+            </div>
+          
         </div>
       </Box>
     </>
