9dd88b4ba907a69b8eb5faa7a29e5462891bd3b9
trungnguyen32182 <138280625+trungnguyen32182@users.noreply.github.com>
2023-11-17 15:55:28 +0700

    'edit ui-ux and logic for product-detail-images'

--- src/components/product/detail/InforImage.js
+++ src/components/product/detail/InforImage.js
@@ -1,9 +1,11 @@
 "use client";
-import { Box } from "@mui/material";
+import { Box, Button, Stack } from "@mui/material";
 import Image from "next/image";
-
-import { useState } from "react";
-
+import { useState, useRef } from "react";
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
+import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
 export default function InforImage({
   dataProduct,
   setActiveImage,
@@ -13,11 +15,67 @@ export default function InforImage({
     dataProduct?.product_images || []
   );
 
+  const [isScrollUp, setScrollUp] = useState(true)
+  const [isScrollDown, setScrollDown] = useState(false)
+  const [isGoPrev, setGoPrev] = useState(true)
+  const [isGoNext, setGoNext] = useState(false)
+  const [isCount, setCount] = useState(0)
+  const containerRef = useRef(null);
+
   const imageLoader = ({ src, width, quality }) => {
     const url = new URL(src);
     url.searchParams.set("width", width.toString());
     return url.href;
   };
+
+  const scrollUpSlider = () => {
+    containerRef.current.scrollTop -= 50
+    if (Math.ceil(containerRef.current.scrollTop <= 50)) {
+      setScrollUp(true)
+    }
+    else {
+      setScrollDown(false)
+      setScrollUp(false)
+    }
+  }
+
+  const scrollDownSlider = () => {
+    containerRef.current.scrollTop += 50
+    const sliderScrollHeight = containerRef.current.scrollHeight - containerRef.current.clientHeight;
+    if (Math.ceil(containerRef.current.scrollTop) >= sliderScrollHeight - 50) {
+      setScrollDown(true)
+    }
+    else {
+      setScrollDown(false)
+      setScrollUp(false)
+    }
+  }
+
+  const goPrev = () => {
+    const value = isCount - 1
+    setCount(value)
+    setActiveImage(imageSliders[value])
+    scrollUpSlider()
+    if (value <= 0) {
+      setGoPrev(true)
+    } else {
+      setGoNext(false)
+      setGoPrev(false)
+    }
+  }
+
+  const goNext = () => {
+    const value = isCount + 1
+    setCount(value)
+    setActiveImage(imageSliders[value])
+    scrollDownSlider()
+    if (value == imageSliders.length - 1) {
+      setGoNext(true)
+    } else {
+      setGoNext(false)
+      setGoPrev(false)
+    }
+  }
 
   return (
     <>
@@ -30,88 +88,201 @@ export default function InforImage({
               width: { xs: "100%", md: "55%" },
             }}
           >
-            <Box
+            <Stack
               sx={{
-                padding: "0 1rem",
-
-                maxHeight: "50rem",
-                overflowY: "auto",
-              }}
-            >
+                justifyContent: 'space-between',
+                alignItems: 'center'
+              }}>
               <Box
+                ref={containerRef}
                 sx={{
-                  display: "flex",
-                  gap: "1rem",
-                  flexDirection: "column",
+                  padding: "0 1rem",
+                  maxHeight: "40rem",
+                  overflowY: "auto",
+                  '- ms - overflow - style': 'none',
+                  scrollbarWidth: 'none',
+                  scrollBehavior: 'smooth',
+                  "&::-webkit-scrollbar": {
+                    display: "none",
+                  },
+                  borderBottom: '.2rem solid #ededed',
+                  paddingBottom: '1rem'
                 }}
               >
-                {imageSliders.map((item, i) => (
-                  <Box
-                    sx={{
-                      padding: "1rem",
-
-                      border: "2px solid",
-                      borderColor: "transparent",
-                      "&.active": {
-                        borderColor: "#7A7272",
-                      },
-                    }}
-                    className={activeImage === item ? "active" : null}
-                    key={i}
-                  >
+                <Box
+                  sx={{
+                    display: "flex",
+                    gap: "1rem",
+                    flexDirection: "column",
+                  }}
+                >
+                  {imageSliders.map((item, i) => (
                     <Box
-                      onClick={() => setActiveImage(item)}
                       sx={{
-                        width: "4.5rem",
-                        height: "4.5rem",
-
-                        position: "relative",
-                        cursor: "pointer",
                         padding: "1rem",
+                        border: "2px solid",
+                        borderColor: "transparent",
+                        "&.active": {
+                          borderColor: "#7A7272",
+                        },
                       }}
+                      className={activeImage === item ? "active" : null}
+                      key={i}
                     >
-                      <Image
-                        alt={dataProduct.product_name}
-                        src={item}
-                        fill
-                        sizes="500"
-                        style={{
-                          width: "100%",
-                          maxWidth: "100%",
-                          objectFit: "contain",
+                      <Box
+                        onClick={(e) => {
+                          const rect = e.currentTarget.getBoundingClientRect();
+                          const offsetTop = rect.top + window.scrollY;
+                          const containerHeight = containerRef.current.clientHeight;
+                          const scrollTopValue = offsetTop - containerHeight / 2;
+                          containerRef.current.scrollTop = scrollTopValue;
+                          setActiveImage(item)
+                          setCount(i)
                         }}
-                      />
+                        sx={{
+                          width: "4.5rem",
+                          height: "4.5rem",
+                          position: "relative",
+                          cursor: "pointer",
+                          padding: "1rem",
+                        }}
+                      >
+                        <Image
+                          alt={dataProduct.product_name}
+                          src={item}
+                          fill
+                          sizes="500"
+                          style={{
+                            width: "100%",
+                            maxWidth: "100%",
+                            objectFit: "contain",
+                          }}
+                        />
+                      </Box>
                     </Box>
-                  </Box>
-                ))}
+                  ))}
+                </Box>
               </Box>
-            </Box>
+              <Box sx={{
+                maxHeight: '8rem',
+                maxWidth: '70%',
+                display: 'flex',
+                flexDirection: 'column',
+              }}>
+                <Button sx={{
+                  minWidth: ' .5rem',
+                  lineHeight: '1.5rem',
+                  backgroundColor: '#fdfdfd',
+                  color: '#767676',
+                  opacity: .9,
+                  '&:hover': {
+                    backgroundColor: '#767676',
+                    color: '#fdfdfd'
+                  }
+                }}
+                  disabled={isScrollUp}
+                  onClick={() => scrollUpSlider()}
+                ><KeyboardArrowUpIcon /></Button>
+                <Button sx={{
+                  minWidth: ' .5rem',
+                  lineHeight: '1.5rem',
+                  backgroundColor: '#fdfdfd',
+                  color: '#767676',
+                  opacity: .9,
+                  '&:hover': {
+                    backgroundColor: '#767676',
+                    color: '#fdfdfd'
+                  }
+                }}
+                  disabled={isScrollDown}
+                  onClick={() => scrollDownSlider()}
+                ><KeyboardArrowDownIcon /></Button>
+              </Box>
+            </Stack>
             <Box
+              className="slider"
               sx={{
                 maxWidth: { xs: "100%", md: "50rem" },
                 flex: 1,
                 height: "50rem",
                 width: "100%",
-
                 position: "relative",
+                overflowX: 'hidden'
               }}
             >
-              <Image
-                src={activeImage}
-                loader={(props) => imageLoader({ ...props, width: 750 })}
-                alt={dataProduct.product_name}
-                fill
-                sizes="1000"
-                style={{
-                  maxWidth: "100%",
-                  height: "100%",
-                  objectFit: "contain",
-                }}
-              />
+
+              {imageSliders.map((item, i) => (
+                <Image
+                  key={i}
+                  src={item}
+                  loader={(props) => imageLoader({ ...props, width: 750 })}
+                  alt={dataProduct.product_name}
+                  fill
+                  sizes="1000"
+                  style={{
+                    position: 'absolute',
+                    maxWidth: "100%",
+                    height: "100%",
+                    objectFit: "contain",
+                    left: `${i * 100}%`,
+                    transition: '1s',
+                    transform: `translateX(-${isCount * 100}%)`,
+                  }}
+                />
+              ))}
+
+              <Button sx={{
+                position: 'absolute',
+                left: 0,
+                top: '50%',
+                transform: 'translateY(-50%)',
+                minWidth: '1rem',
+                minHeight: '1rem',
+                maxWidth: ' 3.5rem',
+                maxHeight: '3.5rem',
+                lineHeight: 0,
+                border: '.1rem solid #ccc',
+                backgroundColor: '#fdfdfd',
+                color: '#767676',
+                opacity: .9,
+                '&:hover': {
+                  backgroundColor: '#767676',
+                  color: '#fdfdfd'
+                }
+              }}
+                disabled={isGoPrev}
+                onClick={() => goPrev()}>
+                <KeyboardArrowLeftIcon />
+              </Button>
+              <Button sx={{
+                position: 'absolute',
+                right: 0,
+                top: '50%',
+                transform: 'translateY(-50%)',
+                minWidth: '1rem',
+                minHeight: '1rem',
+                maxWidth: ' 3.5rem',
+                maxHeight: '3.5rem',
+                lineHeight: 0,
+                border: '.1rem solid #ccc',
+                backgroundColor: '#fdfdfd',
+                color: '#767676',
+                opacity: .9,
+                '&:hover': {
+                  backgroundColor: '#767676',
+                  color: '#fdfdfd'
+                }
+              }}
+                disabled={isGoNext}
+                onClick={() => goNext()}>
+                <KeyboardArrowRightIcon />
+              </Button>
             </Box>
-          </Box>
+          </Box >
         </>
-      )}
+      )
+      }
     </>
   );
 }
+
